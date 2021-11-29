d51ebae6cc0d762f09e408b39c8e55be57635387
jisun94 <jisun94lee@gmail.com>
2021-11-29 14:08:52 +0900

    🚧 Create 2d Context to Canvas and Implement drawing lines

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const canvas = document.querySelector('#jsCanvas');
+const ctx = canvas.getContext('2d');
+
+ctx.strokeStyle = '#2c2c2c';
+ctx.lineWidth = 2.5;
+canvas.width = 500;
+canvas.height = 500;
 
 let painting = false;
 
@@ -6,22 +12,29 @@ function stopPainting() {
   painting = false;
 }
 
-function onMouseMOve(event) {
-  const x = event.offsetX;
-  const y = event.offsetY;
-}
-
-function onMouseDown(event) {
+function startPainting() {
   painting = true;
 }
 
-function onMouseUp(event) {
-  stopPainting();
+function onMouseMOve(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
+
+// function onMouseDown(event) {
+//   painting = true;
+// }
 
 if (canvas) {
   canvas.addEventListener('mousemove', onMouseMOve);
-  canvas.addEventListener('mousedown', onMouseDown);
-  canvas.addEventListener('mouseup', onMouseUp);
+  canvas.addEventListener('mousedown', startPainting);
+  canvas.addEventListener('mouseup', stopPainting);
   canvas.addEventListener('mouseleave', stopPainting);
 }
